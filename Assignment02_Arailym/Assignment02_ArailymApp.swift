a1e0c4c314a11c9ef3463ee8fcc0beaf5d344170
Arailym <arailym.bakenovaa@gmail.com>
2023-09-23 09:21:37 +0600

    Initial Commit

---
+++ Assignment02_Arailym/Assignment02_ArailymApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment02_ArailymApp.swift
+//  Assignment02_Arailym
+//
+//  Created by Арайлым Бакенова on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment02_ArailymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
